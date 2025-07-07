7011fd85d26696f9eab6404603a55202e0c28bac
rezvan <najibirezvan@gmail.com>
2025-07-07 22:19:01 +0330

    style instractur taghir kard

--- src/components/Instructor.jsx
+++ src/components/Instructor.jsx
@@ -2,8 +2,8 @@
 import "../styles/Instructor.css" 
 const Instructor = () => (
   <section className="instructor">
-    <h3 className="text-2xl font-semibold">مدرس درس</h3>
-    <p className="mt-2">مهندس محمدی - عضو هیئت علمی گروه مهندسی کامپیوتر</p>
+    <h3 className="">مدرس درس</h3>
+    <p className="">مهندس محمدی - عضو هیئت علمی گروه مهندسی کامپیوتر</p>
   </section>
 );
 
