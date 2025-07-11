ba6365b8290fe3badf122ccd393592ca53b73cf6
rezvan <najibirezvan@gmail.com>
2025-07-11 17:21:51 +0330

    change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import logo from '../styles/logo.webp'; // مسیر تصویر لوگو
 
-
-const Navbar = () => (
-  <nav className="navbar">
-    <h1 className="">درس وب 2</h1>
-    <div className="navbar-links">
-      <Link to="/">خانه</Link>
-      <Link to="/about">درباره درس</Link>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* بخش چپ: دکمه SEE PROGRAMS */}
+        <div className="navbar-left">
+          <button className="navbar-btn btn-outline">log in</button>
+        </div>
+        
+        {/* بخش وسط: لوگو (تصویر) */}
+        <div className="navbar-center">
+          <Link to="/">
+            <img 
+              src={logo} 
+              alt="Website Logo" 
+              className="navbar-logo-img"
+            />
+          </Link>
+        </div>
+        
+        {/* بخش راست: لینک‌های ناوبری */}
+        <div className="navbar-right">
+          <Link to="./Student" className="navbar-link">students</Link>
+          <Link to="/programs" className="navbar-link">About</Link>
+          <Link to="/contact" className="navbar-link">Home</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
-
